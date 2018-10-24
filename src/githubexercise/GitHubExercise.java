11ec566491ec8ab9dd42cc578a18c3a238370cd1
Hunter Earley <earleyhunter@gmail.com>
2018-10-24 15:51:51 -0400

    This is my commit message, I added my method

--- src/githubexercise/GitHubExercise.java
+++ src/githubexercise/GitHubExercise.java
@@ -5,5 +5,17 @@ public class GitHubExercise
     public static void main(String[] args)
     {
         System.out.println("Henry Thomas");
+        
+        addNums();
+    }
+    
+    public static void addNums(){
+        int first = 2;
+        int second = 8;
+        int result;
+        
+        result = first + second;
+        
+        System.out.println("The answer is: " + result);
     }
 }
